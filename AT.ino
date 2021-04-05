aacbcffc98616f49eb9077980168894a6649460b
Ercan Kırbıyık <69839627+rcnon@users.noreply.github.com>
2021-04-05 23:26:28 +0300

    Update AT.ino

--- AT.ino
+++ AT.ino
@@ -1,9 +1,9 @@
 #include <SoftwareSerial.h>
-SoftwareSerial BTSerial(10,11);
+SoftwareSerial BTSerial(10,11); //Arduino's pins of which bluetooth's Rx and Tx pins connected
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600); 
   Serial.println("Enter AT commands:");
   BTSerial.begin(38400);
 }
